cc45b4d1ff4daf16e20bec852c2f2222de52efb1
nikitabhatta <nikitabhatta27@gmail.com>
2026-06-16 12:34:56 +0545

    Adding new folder pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,9 @@ import "./App.css";
 import MovieCard from "./components/MovieCard"
 
 function App() {
-  const movieNumber = 1;
-
-  
   return (
     <>
-    {movieNumber === 1 ? (
-      <MovieCard movie={{title: "Harry Potter", release_date:"2024"}}/>
-    ) : (
-   <MovieCard movie={{title: "Sunibha", release_date:"2025"}}/>
-    )}
+    
    </>
   );
 }
