c93deea7fcfc3879ead7be40415ced8bc6c84c7d
nikitabhatta <nikitabhatta27@gmail.com>
2026-06-16 12:04:13 +0545

    Initial React Practice

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+import "./App.css";
+import Practice from "./components/Practice"
+
+function App() {
+  return (
+    <>
+   <MovieCard movie={{title: "Harry Potter", release_date:"2024"}}/>
+   </>
+  );
+}
+
+export default App
